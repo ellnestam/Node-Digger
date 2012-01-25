bc8afc1458518e0e095971d83e1164d539f345b5
Ola Ellnestam <ola@agical.se>
2012-01-25 23:11:29 +0100

    Subscribing and publishing possible

--- client/map.js
+++ client/map.js
@@ -1,7 +1,37 @@
+var client = new Faye.Client('http://localhost:8000/pandemic');
+
 function drawMap() {
     var canvas = document.getElementById('myWorld');
     var world = new World();
     world.visualize(new Board());
+}
+
+function subscribe() {
+
+    var subscription = client.subscribe('/move', function(message) {
+	alert("Message was: " + message.text);
+    });
+
+    subscription.callback(function() {
+	alert('Subscription is now active!');
+    });
+
+    subscription.errback(function(error) {
+	alert(error.message);
+    });
+
+}
+
+function publish() {
+    var publication = client.publish('/move', {text: 'Hi there'});
+
+    publication.callback(function() {
+	alert('Message received by server!');
+    });
+
+    publication.errback(function(error) {
+	alert('There was a problem: ' + error.message);
+    });
 }
 
 function Board() {
@@ -12,6 +42,11 @@ Board.prototype.putPlayers = function (players) {
     for (var i = 0; i < players.length; i++) {
 	placePlayerAt(this.canvas, 10, 10);
     }
+}
+
+
+function handleMove(message) {
+    alert(message);
 }
 
 function drawBlackRectangle(canvas, x, y) {
