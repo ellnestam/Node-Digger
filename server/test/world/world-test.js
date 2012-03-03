a41c745fa7a9bfc998f68ff4ee7e98f170c3f4cc
Ola Ellnestam <ola@agical.se>
2012-03-03 16:01:43 +0100

    Rearranged some code

--- server/test/world/world-test.js
+++ server/test/world/world-test.js
@@ -7,6 +7,17 @@ buster.testCase("World behaviour", {
         this.world = world;
     },
 
+    "Parse returns width and height": function () {
+	var field = this.world.parse("ww\nww");
+        buster.assert.equals(field.width, 2);
+        buster.assert.equals(field.height, 2);
+    },
+
+    "": function () {
+	var text = this.world.fileToString('../world/test.field');
+	buster.assert.equals(text, "wwwwww\nwwwwww");
+    },
+
     "World can tell how much gold is present at a point" : function() {
 	var p1 = {x : 1, y : 1};
 	var w = this.world.putGoldAt(p1, 1);
